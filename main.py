57331c41fa91402bf9d212e387d340d3af8b54c0
Ubuntu <ashley@ashley-alec-vm.2hcvuln0bedeljxjdq5fdxbjsc.bx.internal.cloudapp.net>
2020-11-12 19:04:39 +0000

    fix run aprams

--- main.py
+++ main.py
@@ -11,13 +11,13 @@ from train import train_model
 if __name__ == "__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument('--log_suffix', type=str, default = "0")
-    arg_parser.add_argument('--source_dataset', type=str, default = "USPS")
-    arg_parser.add_argument('--target_datasets', type=str, default = "SVHN,MNIST")
+    arg_parser.add_argument('--source_dataset', type=str, default = "SVHN")
+    arg_parser.add_argument('--target_datasets', type=str, default = "MNIST")
     arg_parser.add_argument('--n_train_samples', type=int, default = None)
     arg_parser.add_argument('--n_test_samples', type=int, default = None)
     arg_parser.add_argument('--batch_size', type=int, default = 128) 
-    arg_parser.add_argument('--train_epochs', type=int, default = 1)
-    arg_parser.add_argument('--adapt_epochs', type=int, default = 1)
+    arg_parser.add_argument('--train_epochs', type=int, default = 30)
+    arg_parser.add_argument('--adapt_epochs', type=int, default = 1000)
     args = arg_parser.parse_args()
 
     logger = Logger("_".join([args.source_dataset, 
